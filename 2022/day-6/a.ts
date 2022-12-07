e09017324de74d75f1a45b3f50d8a7f6dd7cb2fc
Waaiez Kinnear <waaiez.kinnear10@gmail.com>
2022-12-08 00:47:17 +0200

    add 2022/day-6

---
+++ 2022/day-6/a.ts
@@ -0,0 +1,18 @@
+export const x = '';
+
+const input = await Deno.readTextFile('./input.txt');
+
+for (let i = 0; i < input.length - 3; i++) {
+	const arr = input.slice(i, i + 4).split('');
+
+	const hasDuplicates = new Set(arr).size !== arr.length;
+
+	if (!hasDuplicates) {
+		console.log(
+			i + 4,
+			'characters need to be processed before the first start-of-packet marker is detected'
+		);
+
+		break;
+	}
+}
